0621834438d3d37de4350def83358062fb150410
Bryan Lopez <bryan.lopez@berkeley.edu>
2019-10-05 01:08:48 -0700

    Created Billboard Remote Branch; added billboard_setup.py

---
+++ billboard/billboard_setup.py
@@ -0,0 +1,18 @@
+'''
+    Billboard .csv Set-Up
+
+    This script will pull Billboard Data using the billboard.py webscraper.
+    GOAL: Create a 1 year, 3 year, and 5 year log of the billboard 'top-100' charts.
+
+    @ TODO Complete date sectioning
+    @ TODO Pull Data for 1 year charts. Save to csv
+    @ TODO Pull Data for 3 year charts. Save to csv
+    @ TODO Pull Data for 5 year charts. Save to csv
+'''
+
+# Imports
+
+import setup
+import pandas as pd
+
+# Code
