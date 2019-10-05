dc67900a7b72e4ddcfd3f582ebbbd4a06fb589e2
Bryan Lopez <bryan.lopez@berkeley.edu>
2019-10-05 12:59:38 -0700

    Reframed Chart Pulling to be more efficient in Setup.py
    - Split import_chart into two functions: import_chart and parse_chart
    - Parse_chart returns cleaned columns with date added

--- setup.py
+++ setup.py
@@ -2,14 +2,46 @@ import billboard
 import json
 import pandas as pd
 
-def import_chart(date=None):
+def import_chart(name='hot-100', date=None, fetch=None, timeout=None):
+    '''
+    Chart importing using the billboard.py HTML parser
 
-    chart = billboard.ChartData('hot-100', date=date)
+    '''
 
-    chart_json = json.loads(chart.json())
+    chart = billboard.ChartData(name=name, date=date, fetch=fetch, timeout=timeout)
 
-    chart_df = pd.DataFrame(chart_json)
+    return chart
+    # chart = billboard.ChartData(name=name, date=date)
+    #
+    # chart_json = json.loads(chart.json())
+    #
+    # chart_df = pd.DataFrame(chart_json)
+    #
+    # chart_df = pd.DataFrame(chart_df['entries'].array)
+    #
+    # return chart_df
 
-    chart_df = pd.DataFrame(chart_df['entries'].array)
+def chart_parser(chart):
+    '''
+    Chart Parser. Returns Pandas DataFrame object
 
-    return chart_df
+    Returns:
+        chart_df - Pandas DataFrame
+            .date - YYYY-MM-DD
+            .artist - String
+            .title - String
+            .lastPos - Int64
+            .rank - Int64
+            .weeks - Int64
+    '''
+
+    chart_entries = json.loads(today.json()) # Load from JSON object
+    chart_entries = pd.DataFrame.from_dict(chart_entires['entries'])
+
+    # Clean Entries
+    chart_entries = chart_entries[['artist', 'title', 'lastPos', 'rank', 'weeks']]
+    chart_entries['date'] = chart.date # Add Date to all rows
+
+    return chart_entries
+
+    
